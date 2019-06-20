aeae2748c0e1d06790b1c366bc75012cc2416f3a
Not A Hero <krsnvijay@gmail.com>
2019-06-20 15:50:45 +0530

    Delete duplicate files from previous commit

--- src/components/about.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import Layout from "../components/layout"
-
-export default () => (
-  <Layout>
-    <h1>About Footsteps</h1>
-    <p>let's you make your learning path and inspire others to follow them</p>
-  </Layout>
-)
