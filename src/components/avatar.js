c4bdae111a97efbc16010875ed985c68c4e7bfa2
Not A Hero <krsnvijay@gmail.com>
2019-06-25 17:06:13 +0530

    Add brand logo to navbar

--- src/components/avatar.js
+++ src/components/avatar.js
@@ -3,7 +3,7 @@ import avatarStyles from "./avatar.module.css"
 export default ({ imageURL, username }) => {
   return (
     <section>
-      <img src={imageURL} alt={username} />
+      <img className={avatarStyles.avatar} src={imageURL} alt={username} />
       <h3>{username}</h3>
     </section>
   )
--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
 import headerStyles from "./header.module.css"
+import brandLogo from "../images/brand-logo.png"
 export default () => (
   <header>
     <nav>
+      <img className={headerStyles.logo} src={brandLogo} />
       <Link to={"/"}>Home</Link>
       <Link to={"/about/"}>About</Link>
       <Link to={"/auth/"}>Auth</Link>
