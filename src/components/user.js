6fa8f97171332882018ca5ce8c35b3576ae5e013
Not A Hero <krsnvijay@gmail.com>
2019-06-26 21:59:48 +0530

    Add basic styling to profile page

---
+++ src/components/user.js
@@ -0,0 +1,63 @@
+import React from "react"
+import Footsteps from "./footsteps"
+import userStyles from "./user.module.css"
+
+export default ({ data }) => {
+  return (
+    <>
+      <h1>My Profile</h1>
+      <section className={userStyles.flexRow}>
+        <section className={userStyles.flexColumn}>
+          <img
+            className={userStyles.avatar}
+            src={data.profilePic}
+            alt={data.username}
+          />
+          <span>
+            <strong>{data.username}</strong>
+          </span>
+        </section>
+        <section className={userStyles.flexColumn}>
+          <h3>{`${data.firstName} ${data.lastName}`}</h3>
+          <ul>
+            {data.tags.map(tag => (
+              <li>{tag}</li>
+            ))}
+          </ul>
+          <h5>{data.about}</h5>
+          <table>
+            <tr>
+              <td>Followers</td>
+              <td>Following</td>
+            </tr>
+            <tr>
+              <td>{data.followers}</td>
+              <td>{data.following}</td>
+            </tr>
+          </table>
+        </section>
+        <ul className={userStyles.flexColumn}>
+          {Object.entries(data.social).map(([key, value]) => (
+            <li>
+              <a href={value}>{key}</a>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section>
+        <h3>My Core Skills:</h3>
+        <ul>
+          {data.coreSkills.map(skill => (
+            <li>{skill}</li>
+          ))}
+        </ul>
+        <h3>Professional Bio:</h3>
+        <p>{data.bio}</p>
+        <p>Your footsteps will be shown here</p>
+      </section>
+      <section>
+        <Footsteps footsteps={data.footsteps} />
+      </section>
+    </>
+  )
+}
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react"
 import Layout from "../components/layout"
-import Avatar from "../components/avatar"
-import Footsteps from "../components/footsteps"
+import User from "../components/user"
 //http://www.mocky.io/v2/5d122c093100002ec508d1b4
 export default () => {
   const [data, setData] = useState(null)
@@ -15,51 +14,8 @@ export default () => {
   }
   useEffect(() => {
     fetchData()
-  })
-  if (data == null) {
-    return (
-      <Layout>
-        <h1>Loading...</h1>
-      </Layout>
-    )
-  } else
-    return (
-      <Layout>
-        <h1>My Profile</h1>
-        <Avatar
-          imageURL={data.profilePic}
-          name={data.firstName}
-          username={data.username}
-        />
-        <ul>
-          {data.tags.map(tag => (
-            <li>{tag}</li>
-          ))}
-        </ul>
-        <dl>
-          <dt>Followers:</dt>
-          <dd>{data.followers}</dd>
-          <dt>Following:</dt>
-          <dd>{data.following}</dd>
-        </dl>
-        <h3>Social:</h3>
-        <ul>
-          {Object.entries(data.social).map(([key, value]) => (
-            <li>
-              <strong>{key}</strong> {value}
-            </li>
-          ))}
-        </ul>
-        <h3>My Core Skills:</h3>
-        <ul>
-          {data.coreSkills.map(skill => (
-            <li>{skill}</li>
-          ))}
-        </ul>
-        <h3>Professional Bio:</h3>
-        <p>{data.bio}</p>
-        <p>Your footsteps will be shown here</p>
-        <Footsteps footsteps={data.footsteps} />
-      </Layout>
-    )
+  }, [])
+  return (
+    <Layout>{data == null ? <h1>Loading...</h1> : <User data={data} />}</Layout>
+  )
 }
