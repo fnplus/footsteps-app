c9af3c2a30ee86310c7e23992c92f8e31f9962e8
Dev <dev.91096@gmail.com>
2019-07-11 20:16:52 +0530

    Update Search bar

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,18 +1,27 @@
 import React from "react"
 import searchbarStyles from "../styles/searchbar.module.css"
 export default () => (
-  <div className={searchbarStyles.container}>
-    <input
-      type="search"
-      name="q"
-      placeholder="Search Footsteps"
-      aria-label="Search"
-    />
-    <select>
-      <option>All</option>
-      <option>Beginner</option>
-      <option>Intermediate</option>
-      <option>Advanced</option>
-    </select>
+  <div
+    style={{
+      display: "block",
+      marginLeft: "auto",
+      marginRight: "auto",
+      width: "95%",
+    }}
+  >
+    <div className={searchbarStyles.container}>
+      <input
+        type="search"
+        name="q"
+        placeholder="Search Footsteps"
+        aria-label="Search"
+      />
+      <select>
+        <option>All</option>
+        <option>Beginner</option>
+        <option>Intermediate</option>
+        <option>Advanced</option>
+      </select>
+    </div>
   </div>
 )
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react"
+
 import Layout from "../components/layout"
 import User from "../components/user"
-//http://www.mocky.io/v2/5d122c093100002ec508d1b4
+
 export default () => {
   const [data, setData] = useState(null)
   const fetchData = async () => {
