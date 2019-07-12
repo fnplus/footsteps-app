94d8b653c2e732b980baa3e1d208bb91cb9f5754
Dev <dev.91096@gmail.com>
2019-07-12 13:11:29 +0530

    Update Footsteps card

--- src/components/User/footstepsCard.js
+++ src/components/User/footstepsCard.js
@@ -1,15 +1,75 @@
 import React, { Component } from "react"
+import { Row, Col } from "antd"
 
 import styles from "../../styles/footsteps.module.css"
 
 export class footstepsCard extends Component {
+  state = {
+    resourceTitle: "",
+    resourceLink: "",
+    iconLink: "",
+    video: false,
+    documentation: false,
+  }
+
+  componentDidMount() {
+    console.log(this.props.footstep)
+    if (this.props.footstep.resourceType === "Video") {
+      this.setState({
+        resourceTitle: "Video Tutorial",
+        resourceLink: "Watch Video",
+        video: true,
+      })
+    } else if (
+      this.props.footstep.resourceType === "Website" ||
+      this.props.footstep.resourceType === "Documentation"
+    ) {
+      this.setState({
+        resourceTitle: "Documentation",
+        resourceLink: "Visit Website",
+        documentation: true,
+      })
+    }
+  }
+
   render() {
     const data = this.props.footstep
 
     return (
       <div className={styles.cardContainer}>
         <img className={styles.icon} src={data.resourceIcon} alt="" />
-        <div>{data.title}</div>
+
+        <div className={styles.detailsContainer}>
+          <div className={styles.title}>{data.title}</div>
+          <div className={styles.description}>{data.description}</div>
+        </div>
+
+        <Row className={styles.typeContainer}>
+          <Col span={4}>
+            {this.state.documentation ? (
+              <img
+                className={styles.typeImage}
+                src={require("../../images/footsteps/internet.png")}
+                alt=""
+              />
+            ) : (
+              ""
+            )}
+
+            {this.state.video ? (
+              <img
+                className={styles.typeImage}
+                src={require("../../images/footsteps/video.png")}
+                alt=""
+              />
+            ) : (
+              ""
+            )}
+          </Col>
+          <Col className={styles.typeTitle} span={20}>
+            {this.state.resourceTitle}
+          </Col>
+        </Row>
       </div>
     )
   }
