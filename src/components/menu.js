44e8846a9f31150cc850e18f79dd6f3c9cba696d
Dev <dev.91096@gmail.com>
2019-07-28 17:59:45 +0530

    Update NavBar

--- src/components/menu.js
+++ src/components/menu.js
@@ -36,61 +36,49 @@ class NavMenu extends Component {
   render() {
     return (
       <div style={this.props.show ? {} : { display: "none" }}>
-        <Menu mode={this.props.mode} style={{ float: "right", border: "none" }}>
-          <Item className={styles.menuItems}>
-            <Link to="/">Home</Link>
-          </Item>
-
-          <Item className={styles.menuItems}>
-            <Link to="/about/">About</Link>
-          </Item>
-
-          <Item className={styles.menuProfile}>
+        <ul>
+          <li className={styles.menuProfile}>
             <img
               className={styles.menuProfileImg}
               src={this.props.user.profile_pic}
               alt=""
               onClick={this.expandMenu}
             />
-          </Item>
+          </li>
 
-          <div
-            className={
-              this.state.expand
-                ? styles.dropDown + " " + styles.dropDownShow
-                : styles.dropDown
-            }
-          >
-            <Row className={styles.dropDownInfo}>
-              <Col span={6}>
-                <img src={this.props.user.profile_pic} alt="" />
-              </Col>
-              <Col span={18}>
-                <h3>
-                  {this.props.user.first_name} {this.props.user.last_name}
-                </h3>
-                <h5>{this.props.user.email}</h5>
-                <div className={styles.profileBtn} onClick={this.profileClick}>
-                  My Profile
-                </div>
-              </Col>
-            </Row>
-            <div className={styles.logoutBtn} onClick={this.logoutClick}>
-              Logout
-            </div>
+          <li className={styles.menuItems}>
+            <Link to="/about/">About</Link>
+          </li>
+          <li className={styles.menuItems}>
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
 
-            {/* <div className={styles.dropDownItem} onClick={this.profileClick}>
-              Profile
-            </div>
-            <div
-              className={styles.dropDownItem}
-              style={{ color: "#fe5e44" }}
-              onClick={this.logoutClick}
-            >
-              Logout
-            </div> */}
+        <div
+          className={
+            this.state.expand
+              ? styles.dropDown + " " + styles.dropDownShow
+              : styles.dropDown
+          }
+        >
+          <Row className={styles.dropDownInfo}>
+            <Col span={6}>
+              <img src={this.props.user.profile_pic} alt="" />
+            </Col>
+            <Col span={18}>
+              <h3>
+                {this.props.user.first_name} {this.props.user.last_name}
+              </h3>
+              <h5>{this.props.user.email}</h5>
+              <div className={styles.profileBtn} onClick={this.profileClick}>
+                My Profile
+              </div>
+            </Col>
+          </Row>
+          <div className={styles.logoutBtn} onClick={this.logoutClick}>
+            Logout
           </div>
-        </Menu>
+        </div>
       </div>
     )
   }
