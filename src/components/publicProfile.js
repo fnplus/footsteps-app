64469ad05293f14863a5c7fc8057d3af1af17f09
Dev <dev.91096@gmail.com>
2019-08-03 12:31:44 +0530

    Unknown username handle

--- src/components/publicProfile.js
+++ src/components/publicProfile.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react"
 import { Query } from "react-apollo"
 import gql from "graphql-tag"
+import { Icon } from "antd"
+import { navigate } from "gatsby"
 
 import Header from "./header"
 import Footer from "./footer"
 import User from "./user"
 import Loader from "./loader"
+
+import styles from "../styles/layout.module.css"
 
 export class publicProfile extends Component {
   render() {
@@ -18,24 +22,64 @@ export class publicProfile extends Component {
           if (loading)
             return (
               <div>
-                <Header show={false} />
-                <Loader />
+                <div className={styles.content}>
+                  <Header show={false} />
+                  <Loader />
+                </div>
                 <Footer />
               </div>
             )
           if (error)
             return (
               <div>
-                <Header show={false} />
-                <h1>Error Loading User</h1>
+                <div className={styles.content}>
+                  <Header show={false} />
+                  <h1>Error Loading User</h1>
+                </div>
                 <Footer />
               </div>
             )
           return (
             <div>
-              <Header show={false} />
-              <User data={data.User[0]} />
-              <Footer />
+              {data.User.length !== 0 ? (
+                <div>
+                  <div className={styles.content}>
+                    <Header show={false} />
+                    <User data={data.User[0]} />
+                  </div>
+                  <Footer />
+                </div>
+              ) : (
+                <div>
+                  <div className={styles.content}>
+                    <Header show={false} />
+                    <div>
+                      <h1 className={styles.public_not_found}>
+                        No profile found with username{" "}
+                        <span className={styles.public_not_found_username}>
+                          {this.props.username}
+                        </span>
+                      </h1>
+                      <div
+                        style={{
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                        }}
+                      >
+                        <div
+                          className={styles.public_not_found_go_home}
+                          onClick={() => navigate("/")}
+                        >
+                          <Icon type="home" style={{ marginRight: "10px" }} />{" "}
+                          Go Home
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <Footer />
+                </div>
+              )}
             </div>
           )
         }}
