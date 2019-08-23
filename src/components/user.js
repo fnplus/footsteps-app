ce4dd103a0e9f58275549a99c30fb1610e73aee3
Dev <dev.91096@gmail.com>
2019-08-23 09:24:08 +0530

    Remove followers and following from profile

--- src/components/user.js
+++ src/components/user.js
@@ -6,36 +6,8 @@ import LearningPaths from "./User/learningPaths"
 import Follow from "./User/follow"
 
 export class user extends Component {
-  state = {
-    footsteps: true,
-    followers: false,
-    following: false,
-  }
-
   componentDidMount() {
     console.log(this.props.data)
-  }
-
-  changeView = title => {
-    if (title === "footsteps") {
-      this.setState({
-        footsteps: true,
-        followers: false,
-        following: false,
-      })
-    } else if (title === "following") {
-      this.setState({
-        footsteps: false,
-        followers: false,
-        following: true,
-      })
-    } else if (title === "followers") {
-      this.setState({
-        footsteps: false,
-        followers: true,
-        following: false,
-      })
-    }
   }
 
   render() {
@@ -220,63 +192,8 @@ export class user extends Component {
 
         <Row className={styles.menuContainer}>
           <Col span={24}>
-            <div className={styles.menu}>
-              <Row>
-                <Col
-                  onClick={() => this.changeView("footsteps")}
-                  xs={8}
-                  lg={6}
-                  className={
-                    this.state.footsteps
-                      ? styles.menuItem + " " + styles.active
-                      : styles.menuItem
-                  }
-                >
-                  Footsteps
-                </Col>
-                <Col
-                  onClick={() => this.changeView("following")}
-                  xs={8}
-                  lg={6}
-                  className={
-                    this.state.following
-                      ? styles.menuItem + " " + styles.active
-                      : styles.menuItem
-                  }
-                >
-                  Following
-                </Col>
-                <Col
-                  onClick={() => this.changeView("followers")}
-                  xs={8}
-                  lg={6}
-                  className={
-                    this.state.followers
-                      ? styles.menuItem + " " + styles.active
-                      : styles.menuItem
-                  }
-                >
-                  Followers
-                </Col>
-              </Row>
-            </div>
-
             <div className={styles.content}>
-              {this.state.footsteps ? (
-                <LearningPaths learning_paths={data.learning_paths} />
-              ) : (
-                ""
-              )}
-              {this.state.followers ? (
-                <Follow users={data.followers} type={false} />
-              ) : (
-                ""
-              )}
-              {this.state.following ? (
-                <Follow users={data.following} type={true} />
-              ) : (
-                ""
-              )}
+              <LearningPaths learning_paths={data.learning_paths} />
             </div>
           </Col>
         </Row>
