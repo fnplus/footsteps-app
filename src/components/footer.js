5934acbe1a1d675bd34e8add1e0c6057a656811e
Dev <dev.91096@gmail.com>
2019-08-28 07:48:45 +0530

    Update Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,24 +1,40 @@
 import React, { Component } from "react"
+import { Row, Col } from "antd"
 
 import styles from "../styles/footer.module.css"
 
 export class footer extends Component {
   render() {
     return (
-      <footer>
-        Made with{" "}
-        <span role="img" aria-label="heart">
-          ❤️
-        </span>{" "}
-        by{" "}
-        <a
-          className={styles.OS_link}
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/fnplus/footsteps-app"
-        >
-          Fnplus
-        </a>
+      <footer className={styles.container}>
+        <Row>
+          <Col xs={24} lg={12}>
+            Made with{" "}
+            <span role="img" aria-label="heart">
+              ❤️
+            </span>{" "}
+            by{" "}
+            <a
+              className={styles.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://fnplus.tech"
+            >
+              Fnplus
+            </a>
+          </Col>
+          <Col xs={24} lg={12} className={styles.footer_right}>
+            Check out this project on{" "}
+            <a
+              className={styles.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/fnplus/footsteps-app"
+            >
+              Github
+            </a>{" "}
+          </Col>
+        </Row>
       </footer>
     )
   }
