de18ef0b1b9959aa96c1393543737f259e294917
dev <dev.91096@gmail.com>
2019-10-02 20:15:58 +0530

    Update profile route

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -7,115 +7,13 @@ import gql from "graphql-tag"
 import Loader from "../components/loader"
 
 export class profile extends Component {
-  state = {
-    id: "",
-  }
-
-  componentDidMount() {
-    if (typeof window !== undefined) {
-      this.setState({
-        id: localStorage.getItem("userId"),
-      })
-    }
-  }
-
   render() {
     return (
-      <Query query={USERS_QUERY_APOLLO} variables={{ id: this.state.id }}>
-        {({ data, loading, error }) => {
-          if (loading)
-            return (
-              <Layout>
-                <Loader />
-              </Layout>
-            )
-          if (error)
-            return (
-              <Layout>
-                <h1>Error Loading User</h1>
-              </Layout>
-            )
-          return (
-            <Layout>
-              <User data={data.User} />
-            </Layout>
-          )
-        }}
-      </Query>
+      <Layout>
+        <User />
+      </Layout>
     )
   }
 }
 
 export default profile
-
-export const USERS_QUERY_APOLLO = gql`
-  query getuser($id: uuid!) {
-    User: Users_by_pk(id: $id) {
-      id
-      first_name
-      last_name
-      username
-      skills
-      profile_pic
-      following_aggregate {
-        aggregate {
-          count
-        }
-      }
-      following {
-        follower_info {
-          username
-          first_name
-          last_name
-          profile_pic
-          id
-        }
-      }
-      followers_aggregate {
-        aggregate {
-          count
-        }
-      }
-      followers {
-        follow_info {
-          username
-          profile_pic
-          first_name
-          last_name
-          id
-        }
-      }
-      about
-      bio
-      linkedin
-      github
-      facebook
-      twitter
-      learning_paths {
-        id
-        title
-        description
-        icon
-        votes_aggregate {
-          aggregate {
-            count
-          }
-        }
-        footsteps {
-          id
-          title
-          resource_icon
-          resource_url
-          resource_type
-          level
-          description
-        }
-      }
-      learning_paths_aggregate {
-        aggregate {
-          count
-        }
-      }
-    }
-  }
-`
