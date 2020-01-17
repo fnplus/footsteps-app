4927b0461ecfc8967627d6b2109e3cda795e5a22
aakhan <adnank122@gmail.com>
2020-01-17 14:32:22 +0500

    Converted SideDrawer to use hooks

--- src/components/Layout/sideDrawer.js
+++ src/components/Layout/sideDrawer.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 import { Drawer, Row, Col } from "antd"
 import { navigate } from "gatsby"
 import firebase from "firebase/app"
@@ -7,96 +7,88 @@ import "firebase/auth"
 import styles from "../../styles/header.module.css"
 
 import NavMenu from "./menu"
-export class header extends Component {
-  state = {
-    visible: false,
+export function header({ show = false }) {
+  const [visible, setVisible] = useState(false)
+
+  const showDrawer = () => {
+    setVisible(visible => !visible)
   }
 
-  showDrawer = () => {
-    this.setState({
-      visible: !this.state.visible,
-    })
+  const onClose = () => {
+    setVisible(false)
   }
 
-  onClose = () => {
-    this.setState({
-      visible: false,
-    })
-  }
-
-  logoutClick = () => {
+  const logoutClick = () => {
     firebase.auth().signOut()
     navigate("/")
   }
 
-  render() {
-    return (
-      <nav className={styles.navContainer}>
-        <Row>
-          <Col xs={6} md={2} className={styles.logo}>
-            <img
-              src={require("../../images/brand-logo.png")}
-              alt=""
-              onClick={() => navigate("/")}
-            />
-          </Col>
+  return (
+    <nav className={styles.navContainer}>
+      <Row>
+        <Col xs={6} md={2} className={styles.logo}>
+          <img
+            src={require("../../images/brand-logo.png")}
+            alt=""
+            onClick={() => navigate("/")}
+          />
+        </Col>
 
-          {this.props.show ? (
-            <Col xs={18} md={22}>
-              <div className={styles.menuContainer}>
-                <div className={styles.navDesktop}>
-                  <NavMenu mode="horizontal" user={this.props.user} />
-                </div>
-
-                <div className={styles.mobile_toggle} onClick={this.showDrawer}>
-                  <img src={require("../../images/menu.png")} alt="Menu" />
-                </div>
-
-                <Drawer
-                  placement="right"
-                  closable={false}
-                  onClose={this.onClose}
-                  visible={this.state.visible}
-                  width={200}
-                >
-                  <div className={styles.navItem} onClick={() => navigate("/")}>
-                    Home
-                  </div>
-                  <div
-                    className={styles.navItem}
-                    onClick={() => navigate("/add/")}
-                  >
-                    Create Path
-                  </div>
-                  <div
-                    className={styles.navItem}
-                    onClick={() => navigate("/profile/")}
-                  >
-                    Profile
-                  </div>
-                  <div
-                    className={styles.navItem}
-                    onClick={() => navigate("/settings/")}
-                  >
-                    Settings
-                  </div>
-                  <div
-                    className={styles.navItem}
-                    onClick={this.logoutClick}
-                    style={{ color: "#fe5e44" }}
-                  >
-                    Logout
-                  </div>
-                </Drawer>
+        {show ? (
+          <Col xs={18} md={22}>
+            <div className={styles.menuContainer}>
+              <div className={styles.navDesktop}>
+                <NavMenu mode="horizontal" />
               </div>
-            </Col>
-          ) : (
-            ""
-          )}
-        </Row>
-      </nav>
-    )
-  }
+
+              <div className={styles.mobile_toggle} onClick={showDrawer}>
+                <img src={require("../../images/menu.png")} alt="Menu" />
+              </div>
+
+              <Drawer
+                placement="right"
+                closable={false}
+                onClose={onClose}
+                visible={visible}
+                width={200}
+              >
+                <div className={styles.navItem} onClick={() => navigate("/")}>
+                  Home
+                </div>
+                <div
+                  className={styles.navItem}
+                  onClick={() => navigate("/add/")}
+                >
+                  Create Path
+                </div>
+                <div
+                  className={styles.navItem}
+                  onClick={() => navigate("/profile/")}
+                >
+                  Profile
+                </div>
+                <div
+                  className={styles.navItem}
+                  onClick={() => navigate("/settings/")}
+                >
+                  Settings
+                </div>
+                <div
+                  className={styles.navItem}
+                  onClick={logoutClick}
+                  style={{ color: "#fe5e44" }}
+                >
+                  Logout
+                </div>
+              </Drawer>
+            </div>
+          </Col>
+        ) : (
+          ""
+        )}
+      </Row>
+    </nav>
+  )
 }
 
 export default header
