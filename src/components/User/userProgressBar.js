46e54eebb0425ef23e1d682551d928fd5f73f849
Prajwal Singh <30700921+prajwal714@users.noreply.github.com>
2020-03-12 11:56:26 +0530

    Resolved Merging, Issue User Progress Bar #63 (#90)
    * added progress bar on profile page, issue #63
    
    * removed unnecessary css styles for progress bar
    
    * updated progress bar consecutive steps
    
    * modified progress bar, reduced width, removed progress bar in mobile view
    
    * removed unused imported objects
    
    * reverted temporary changes
    
    * resolved conflict

---
+++ src/components/User/userProgressBar.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import { Steps, Progress } from 'antd';
+const { Step } = Steps;
+
+const UserProgress = ({ data, type }) => {
+    console.log(data);
+    const { about, skills, linkedin, github, facebook } = data;
+
+    const progress = [
+        about, skills, github, linkedin, facebook
+    ]
+
+    let progressVal = 0;
+    for (let i = 0; i < 5; i++) {
+        const ele = progress[i];
+        console.log(ele);
+        if (ele !== "" && ele !== "https://github.com/" && ele !== "https://linkedin.com/in/" && ele !== "https://facebook.com/")
+            progressVal++;
+        else
+            break;
+    }
+    return (
+        <div style={type === "mobile" ? { marginTop: "10px" } : {}}>
+            <h3>Profile Strength</h3>
+            {
+                (type === "mobile") ?
+                    (<>
+                        <Steps
+                            direction="vertical"
+                            current={progressVal}>
+
+                            <Step title="About" />
+                            <Step title="Skills" />
+                            <Step title="Github" />
+                            <Step title="LinkedIn" />
+                            <Step title="Facebook" />
+                        </Steps>
+                    </>) :
+                    (
+                        <>
+                            <Steps
+                                current={progressVal}
+                                style={{ width: "80%" }}
+                            >
+
+                                <Step title="About" />
+                                <Step title="Skills" />
+                                <Step title="Github" />
+                                <Step title="LinkedIn" />
+                                <Step title="Facebook" />
+
+                            </Steps>
+                            <Progress
+                                strokeColor={{
+                                    '0%': '#108ee9',
+                                    '100%': '#87d068',
+                                }}
+                                style={{
+                                    width: "80%"
+                                }}
+                                percent={progressVal * 20}
+                            />
+                        </>
+                    )
+            }
+        </div>
+    );
+}
+
+export default UserProgress;
